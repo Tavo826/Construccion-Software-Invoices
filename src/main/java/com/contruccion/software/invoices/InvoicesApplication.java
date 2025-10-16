97346ed85b2eb37c75416e9fef4fe2fd2146c699
Gustavo Orozco <gustavo.orozcom68@gmail.com>
2025-10-16 14:26:09 -0500

    Project init

---
+++ src/main/java/com/contruccion/software/invoices/InvoicesApplication.java
@@ -0,0 +1,13 @@
+package com.contruccion.software.invoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesApplication.class, args);
+	}
+
+}
